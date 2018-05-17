48baf55693f05c66bc0b9d6191cf44dadeaa0e61
Zhongqiang Shen <shenzhongqiang@msn.com>
2018-05-17 16:34:44 +0800

    implement function to convert python code to png image

---
+++ main.py
@@ -0,0 +1,70 @@
+import os
+import sys
+from selenium import webdriver
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter, ImageFormatter
+
+def get_width(content):
+    lines = content.split("\n")
+    max_width = 0
+    for line in lines:
+        width = len(line)
+        if width > max_width:
+            max_width = width
+    return max_width
+
+def get_height(content):
+    lines = content.split("\n")
+    max_height = len(lines)
+    return max_height
+
+def to_png(html_filepath, code_width, code_height):
+    folder = os.path.dirname(__file__)
+    outpath = os.path.join(folder, "code.png")
+    driver = webdriver.PhantomJS()
+    driver.set_window_size(code_width+60, code_height)
+    driver.get(html_filepath)
+    driver.save_screenshot(outpath)
+    driver.quit()
+    os.remove(html_filepath)
+
+def make_html(style_css, code_html, code_width, code_height):
+    css = "\
+    .rcorner {{ \
+        border-radius: 15px; \
+        background: #272822; \
+        width: {}px; \
+        height: {}px; \
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); \
+        font-size: 14px; \
+        padding: 20px; \
+    }} \
+    .rcorner .highlight {{ \
+    }}".format(code_width, code_height)
+    result_css = style_css + css
+    html = "<!DOCTYPE html><html><head><style>{}</style></head><body><div class=\"rcorner\">{}</div></body></html>".format(result_css, code_html)
+    folder = os.path.dirname(__file__)
+    outpath = os.path.join(folder, "out.html")
+    with open(outpath, "w") as f:
+        f.write(html)
+    return outpath
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: %s <code file>" % sys.argv[0])
+        sys.exit(1)
+
+    filepath = sys.argv[1]
+    with open(filepath) as f:
+        content = f.read()
+        width = get_width(content)
+        height = get_height(content)
+        code_width = width*9 + 30
+        code_height = height*15 + 30
+        formatter = HtmlFormatter(nobackground=True, style="monokai")
+        code_html = highlight(content, PythonLexer(), formatter)
+        style_css = formatter.get_style_defs('.highlight')
+        html_filepath = make_html(style_css, code_html, code_width, code_height)
+        to_png(html_filepath, code_width, code_height)
+
---
+++ sample.py
@@ -0,0 +1,11 @@
+def func(x):
+    return x * x
+
+if __name__ == "__main__":
+    print "a very very very very very very very very very ve"
+    result = func(10)
+    print(result)
+    print(result)
+    print(result)
+    print(result)
+    print(result)
